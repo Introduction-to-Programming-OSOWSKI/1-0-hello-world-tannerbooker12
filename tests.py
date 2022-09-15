2312b2f1a10d32188b35f321d7e70288804ab6c5
tannerbooker12 <829075@mystma.org>
2022-09-15 09:23:23 -0500

    3

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ month = 9
 day = 17
 
 def test_code():
-    assert main.hellowWorld() == "Hello World!", "Your function did not return Hello World!"
+    assert main.helloWorld() == "Hello World!", "Your function did not return Hello World!"
 
 def test_late():
     assert datetime.datetime.now() < datetime.datetime(year, month, day + 1, 4, 0), "Submitted Late"
